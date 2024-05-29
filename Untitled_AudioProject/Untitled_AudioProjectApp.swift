f6492d9778ebb76f8fd349867d5efb336ace77b5
Tiziano Bruni <tiziano.bruni@gmail.com>
2024-05-29 21:19:23 +0100

    First commit

---
+++ Untitled_AudioProject/Untitled_AudioProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Untitled_AudioProjectApp.swift
+//  Untitled_AudioProject
+//
+//  Created by Tiziano Bruni on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Untitled_AudioProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
